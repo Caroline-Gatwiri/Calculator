650c454d45e326b93252a609000e762694e1b610
Caroline-Gatwiri <caroline1gatwiri@gmail.com>
2024-09-13 11:53:44 -0500

    Adding the spacer composable to make the UI look better

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -5,10 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,19 +33,22 @@ class MainActivity : ComponentActivity() {
                 TextField(value = num2, onValueChange = {
                     num2  = it
                 })
-                
-                Button(onClick = {
-                    var result =  num1.toInt() + num2.toInt()
-                    Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
-                }) {
-                    Text(text = "Add")
+                Row {
+                    Button(onClick = {
+                        var result =  num1.toInt() + num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(text = "Add")
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Button(onClick = {
+                        var result =  num1.toInt() - num2.toInt()
+                        Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(text = "Subtract")
+                    }
                 }
-                Button(onClick = {
-                    var result =  num1.toInt() - num2.toInt()
-                    Toast.makeText(applicationContext, "Result is $result", Toast.LENGTH_SHORT).show()
-                }) {
-                    Text(text = "Subtract")
-                }
+
             }
 
         }
